09a489e2f2d741b135a79f0346785f5ef78b339a
fernando-fukunaga <fernandofukunaga14@gmail.com>
2024-08-08 00:38:46 -0300

    Initial commit

---
+++ src/main/java/org/example/groceryadminbackend/SpringBootGroceryAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.groceryadminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGroceryAdminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGroceryAdminBackendApplication.class, args);
+    }
+
+}
